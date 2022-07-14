a93a96e052b1e35eaa6a9eb98846d0a32fcda4d6
Cesar Auguto <cesarapolidorio@gmail.com>
2022-07-14 20:25:34 -0300

    feat: add filters on class Product

--- cloud/functions/product.js
+++ cloud/functions/product.js
@@ -3,13 +3,25 @@ const Product = Parse.Object.extend("Product");
 Parse.Cloud.define("get-product-list", async (req, res) => {
     const queryProduct = new Parse.Query(Product);
 
+    if (req.params.title != null) {
+        // queryProduct.fullText("title", req.params.title);
+        queryProduct.matches("title", ".*" + req.params.title + ".*");
+    }
+
+    const itemsPerPage = req.params.itemsPerPage || 1;
+
+    if (itemsPerPage > 100) throw new Error("Items per page must be less than 100"); 
+
+    queryProduct.skip(itemsPerPage * req.params.page || 0);
+    queryProduct.limit(itemsPerPage);
+
     const result = await queryProduct.find({ useMasterKey: true })
     
     return result.map(function (product) {
         product = product.toJSON();
         return {
             id: product.objectId,
-            name: product.name,
+            title: product.title,
             price: product.price,
             unit: product.unit,
             picture: product.picture != null ? product.picture.url : null,
